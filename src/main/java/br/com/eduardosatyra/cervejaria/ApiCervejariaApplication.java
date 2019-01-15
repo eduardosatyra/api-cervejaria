5d3fe63be5ae281c954196c0b6b2d79fcee84b75
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-01-14 23:02:42 -0200

    inicialização do projeto

---
+++ src/main/java/br/com/eduardosatyra/cervejaria/ApiCervejariaApplication.java
@@ -0,0 +1,14 @@
+package br.com.eduardosatyra.cervejaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCervejariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCervejariaApplication.class, args);
+	}
+
+}
+
