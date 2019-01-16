23196867068409bb1924bffca3a38e37d4c5b591
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-01-15 22:01:12 -0200

    testando persistencia com lista spring 2.0

--- src/main/java/br/com/eduardosatyra/cervejaria/ApiCervejariaApplication.java
+++ src/main/java/br/com/eduardosatyra/cervejaria/ApiCervejariaApplication.java
@@ -1,14 +1,32 @@
 package br.com.eduardosatyra.cervejaria;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.eduardosatyra.cervejaria.domain.Categoria;
+import br.com.eduardosatyra.cervejaria.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ApiCervejariaApplication {
+public class ApiCervejariaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCervejariaApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
 
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
+
+}
