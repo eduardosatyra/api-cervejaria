ec24f781d72e9df8f754aa71deba4969e9f23f9b
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-01-17 01:30:32 -0200

    ItemPedido, ItemPedidoPK

--- src/main/java/br/com/eduardosatyra/cervejaria/ApiCervejariaApplication.java
+++ src/main/java/br/com/eduardosatyra/cervejaria/ApiCervejariaApplication.java
@@ -13,6 +13,7 @@ import br.com.eduardosatyra.cervejaria.domain.Cidade;
 import br.com.eduardosatyra.cervejaria.domain.Cliente;
 import br.com.eduardosatyra.cervejaria.domain.Endereco;
 import br.com.eduardosatyra.cervejaria.domain.Estado;
+import br.com.eduardosatyra.cervejaria.domain.ItemPedido;
 import br.com.eduardosatyra.cervejaria.domain.Pagamento;
 import br.com.eduardosatyra.cervejaria.domain.PagamentoComBoleto;
 import br.com.eduardosatyra.cervejaria.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.com.eduardosatyra.cervejaria.repositories.CidadeRepository;
 import br.com.eduardosatyra.cervejaria.repositories.ClienteRepository;
 import br.com.eduardosatyra.cervejaria.repositories.EnderecoRepository;
 import br.com.eduardosatyra.cervejaria.repositories.EstadoRepository;
+import br.com.eduardosatyra.cervejaria.repositories.ItemPedidoRepository;
 import br.com.eduardosatyra.cervejaria.repositories.PagamentoRepository;
 import br.com.eduardosatyra.cervejaria.repositories.PedidoRepository;
 import br.com.eduardosatyra.cervejaria.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class ApiCervejariaApplication implements CommandLineRunner {
 	PedidoRepository pedidoRepository;
 	@Autowired
 	PagamentoRepository pagamentoRepository;
+	@Autowired
+	ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCervejariaApplication.class, args);
@@ -113,7 +117,39 @@ public class ApiCervejariaApplication implements CommandLineRunner {
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/br/com/eduardosatyra/cervejaria/domain/Pedido.java
+++ src/main/java/br/com/eduardosatyra/cervejaria/domain/Pedido.java
@@ -2,6 +2,8 @@ package br.com.eduardosatyra.cervejaria.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 /**
@@ -37,10 +40,13 @@ public class Pedido implements Serializable {
 	@JoinColumn(name = "endereco_de_entrega_id") // associação direcional
 	private Endereco enderecoDeEntrega;
 
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	public Pedido() {
 	}
 
-	public Pedido(Integer id, Date instante,  Cliente cliente, Endereco enderecoDeEntrega) {
+	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		this.id = id;
 		this.instante = instante;
 		this.cliente = cliente;
@@ -85,6 +91,14 @@ public class Pedido implements Serializable {
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
