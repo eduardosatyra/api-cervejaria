aa32ea9c0eef89dba596ae383bb0fac91f7c2cad
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-01-18 00:09:54 -0200

    inserindo uma nova Categoria com o method POST

--- src/main/java/br/com/eduardosatyra/cervejaria/resources/CategoriaResource.java
+++ src/main/java/br/com/eduardosatyra/cervejaria/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package br.com.eduardosatyra.cervejaria.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.eduardosatyra.cervejaria.domain.Categoria;
 import br.com.eduardosatyra.cervejaria.services.CategoriaService;
@@ -27,6 +31,14 @@ public class CategoriaResource {
 		Categoria categoria = categoriaService.buscar(id);
 
 		return ResponseEntity.ok(categoria);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
