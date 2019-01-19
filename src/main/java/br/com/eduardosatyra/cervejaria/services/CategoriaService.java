895293a6873968df811f450edbc20b7dfbf4fcc6
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-01-19 03:37:52 -0200

    correcoes update em categoriaService e construtor de pagamento

--- src/main/java/br/com/eduardosatyra/cervejaria/services/CategoriaService.java
+++ src/main/java/br/com/eduardosatyra/cervejaria/services/CategoriaService.java
@@ -49,8 +49,9 @@ public class CategoriaService {
 	 * @return
 	 */
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	/**
@@ -82,6 +83,10 @@ public class CategoriaService {
 
 	public Categoria parseToDto(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 
 }
