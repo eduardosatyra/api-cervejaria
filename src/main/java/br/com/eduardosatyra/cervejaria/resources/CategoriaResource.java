ca93752ac7da7e265441463d4eb7577cd44f49cd
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-01-19 01:45:48 -0200

    paginação com parametros opcionais na requisição

--- src/main/java/br/com/eduardosatyra/cervejaria/resources/CategoriaResource.java
+++ src/main/java/br/com/eduardosatyra/cervejaria/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -64,4 +66,15 @@ public class CategoriaResource {
 		return ResponseEntity.ok(categoriaDto);
 	}
 
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBY", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+			//requisiçoes = 'value'?param=paramData&...
+		Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> categoriaDto = list.map(obj -> new CategoriaDTO(obj)); // Page do spring ja é "JAVA 8 COMPLIANCE", não necessita o stream e a converção
+		return ResponseEntity.ok(categoriaDto);
+	}
 }
